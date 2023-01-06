6688ca3ad6842dad0b8d5c755caa10b950e5f3e8
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-01-06 10:43:32 -0700

    add all pseudocode and code

--- challenge1.rb
+++ challenge1.rb
@@ -1,7 +1,9 @@
 # Overall goal = create an array of strings, return only the strings that have exactly 4 characters.
 # Ill be working with arrays, strings, and methods.
 # Create an array of strings.
-# 
+# call the .each method on that array that prints the name of a string if it is exactly 4 characters.
+# use the .length method to check for the number of characters in the string.
+
 
 
 names = ["jan", "alec", "young", "logan", "stephen", "joe", "john"]
